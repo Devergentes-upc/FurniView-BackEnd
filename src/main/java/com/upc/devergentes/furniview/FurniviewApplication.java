40ade2ef954680b6e9ed07020924640a471e08b3
Hans Retes <hans_2801_7@hotmail.com>
2023-05-20 13:45:56 -0500

    feat: add initialized code from spring initializr for FurniView

---
+++ src/main/java/com/upc/devergentes/furniview/FurniviewApplication.java
@@ -0,0 +1,13 @@
+package com.upc.devergentes.furniview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurniviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FurniviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upc/devergentes/furniview/FurniviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upc.devergentes.furniview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FurniviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
